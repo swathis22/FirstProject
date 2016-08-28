a5e86dcceb210c9274adcf235189c80c03f449a9
swathis22 <swathic522@gmail.com>
2016-08-28 20:24:19 +0530

    create FirstProject.java
    My first project in GITHUB

---
+++ FirstProject.java
@@ -0,0 +1,14 @@
+package com;
+import java.util.*;
+import java.lang.*;
+ 
+ public class FirstProject{
+ 
+ public static void main (String []args){
+ 
+ System.out.println("Welcome to First program");
+ 
+ }
+ 
+ 
+ }
